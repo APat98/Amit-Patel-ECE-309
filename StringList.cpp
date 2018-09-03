91b8d81e2d01987d16ef6cac1b98c1967cebd4f1
APat98 <42947660+APat98@users.noreply.github.com>
2018-09-03 18:07:47 -0400

    Update StringList.cpp

--- StringList.cpp
+++ StringList.cpp
@@ -1,22 +1,28 @@
-class ListNode{
-private:
-  Item item;
-  ListNode *next;
-public:
-  ListNode(Item a)
-    { item = a; next=NULL; }
-  ListNode* getNext() { return next; }
-  void setNext(ListNode *n) { next = n; }
-  Item getItem() { return item; }
-  };
+#include <stdio.h>
  
 class List {
 private:
+      class ListNode{
+    private:
+      Item item;
+      ListNode *next;
+    public:
+      ListNode(Item a)
+        { item = a; next=NULL; }
+      ListNode* getNext() { return next; }
+      void setNext(ListNode *n) { next = n; }
+      Item getItem() { return item; }
+      };
+  
   ListNode *head;
   ListNode *tail;
-public:
-  List();
-  void append(Item a);
-  bool remove(Item &a);
+
+  public:
+  char* push_back(string);
+  char* get(n);
+  int length();
+  char* remove_front();
   bool empty();
+  List();
+  ~List();
 };
