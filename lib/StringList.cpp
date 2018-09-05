8ecfb8cf637101c37a2f956ea34f58cac723625a
APat98 <42947660+APat98@users.noreply.github.com>
2018-09-05 15:40:14 -0400

    Update StringList.cpp

--- lib/StringList.cpp
+++ lib/StringList.cpp
@@ -106,10 +106,10 @@ int List::length()
    return i;
 }
 
-ListNode List::*get(int n)
+ListNode* List::get(int n)
 {
   int k;
-  ListNode* nth=head;
+  ListNode *nth=head;
   for(k=0;k<n;k++){
      nth = nth->getNext;
   }
